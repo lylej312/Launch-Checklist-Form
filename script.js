4cac664601daf16ab4c15653cf3c7bc918fb12f6
lylej312 <74841752+lylej312@users.noreply.github.com>
2021-03-30 18:19:27 -0400

    Fetch planetary data and fuel/cargo check added

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 window.addEventListener("load", function () {
-  const formSubmitBtn = document.querySelector("#formSubmit");
+  // Get planetary data
+  fetch("https://handlers.education.launchcode.org/static/planets.json").then(
+    function (response) {
+      response.json().then(function (json) {
+        let randomNum = Math.floor(Math.random() * json.length);
+        let planet = json[randomNum];
+        let planetName = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let image = planet.image;
+        let moons = planet.moons;
 
+        missionTarget.innerHTML = `
+  <h2>Mission Destination</h2>
+  <ol>
+    <li>Name: ${planetName}</li>
+    <li>Diameter: ${diameter}</li>
+    <li>Star: ${star}</li>
+    <li>Distance from Earth: ${distance}</li>
+    <li>Number of Moons: ${moons}</li>
+  </ol>
+  <img src="${image}"></img>`;
+      });
+    }
+  );
+  const formSubmitBtn = document.querySelector("#formSubmit");
   formSubmitBtn.addEventListener("click", function (event) {
     event.preventDefault();
-
     // DOM selection
     const inputs = document.querySelectorAll("input");
     const pilotNameValue = document.querySelector("#pilotName").value;
     const copilotNameValue = document.querySelector("#copilotName").value;
     const fuelLevelValue = document.querySelector("#fuelLevel").value;
     const cargoMassValue = document.querySelector("#cargoMass").value;
-
     const launchStatus = document.querySelector("#launchStatus");
-
     const faultyItems = document.querySelector("#faultyItems");
     const pilotStatus = document.querySelector("#pilotStatus");
     const copilotStatus = document.querySelector("#copilotStatus");
@@ -22,8 +44,8 @@ window.addEventListener("load", function () {
     for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
       if (input.value === "") {
-        //   alert("You must fill in all fields");
-        //   event.preventDefault();
+        alert("You must fill in all fields");
+        event.preventDefault();
         return 0;
       }
     }
@@ -36,45 +58,53 @@ window.addEventListener("load", function () {
     ) {
       alert("You must enter a valid value for each field");
     }
-    // Launch check
+    // Launch status check
     let launchCheck;
-    // Check fuel level
-    let fuelCheck = `high enough`;
-    // If low fuel
-    if (fuelLevelValue < 10000) {
-      faultyItems.style.visibility = "visible";
-      launchStatus.innerHTML = `Shuttle not ready for launch`;
-      launchStatus.style.color = "red";
-      fuelCheck = `too low`;
-    }
-    // Check cargo
-    let cargoCheck = `low enough`;
-    // If high cargo
-    if (cargoMassValue > 10000) {
-      faultyItems.style.visibility = "visible";
-      if (fuelLevelValue < )
-      launchStatus.style.color = "red";
-      cargoCheck = `too high`;
+    let fuelCheckMessage;
+    let fuelCheck = false;
+    let cargoCheck = false;
+
+    if (fuelLevelValue > 10000) {
+      fuelCheck = true;
     } else {
+      fuelCheck = false;
+    }
+    if (cargoMassValue < 10000) {
+      cargoCheck = true;
+    } else {
+      cargoCheck = false;
+    }
+
+    // Check fuel level
+    if (fuelCheck) {
+      fuelCheckMessage = `high enough`;
+    } else {
+      fuelCheckMessage = `too low`;
+    }
+
+    // Check cargo weight
+    if (cargoCheck) {
+      cargoCheckMessage = `low enough`;
+    } else {
+      cargoCheckMessage = `too high`;
+    }
+
+    // Launch status display
+    if (!fuelCheck || !cargoCheck) {
+      launchCheck = `not ready`;
+      launchStatus.style.color = "red";
+      faultyItems.style.visibility = "visible";
+    } else {
+      launchCheck = `ready`;
+      launchStatus.style.color = "green";
+      faultyItems.style.visibility = "visible";
     }
 
     // Faulty Items HTML injections
     pilotStatus.innerHTML = `Pilot ${pilotNameValue} Ready`;
     copilotStatus.innerHTML = `Co-pilot ${copilotNameValue} Ready`;
-    fuelStatus.innerHTML = `Fuel level ${fuelCheck} for launch`;
-    cargoStatus.innerHTML = `Cargo mass ${cargoCheck} for launch`;
+    fuelStatus.innerHTML = `Fuel level ${fuelCheckMessage} for launch`;
+    cargoStatus.innerHTML = `Cargo mass ${cargoCheckMessage} for launch`;
     launchStatus.innerHTML = `Shuttle ${launchCheck} for launch`;
   });
 });
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
